413b38084f8e8954d000ffa0a97563c85fd4165a
Eton Hsu <etonhsu111@gmail.com>
2024-05-05 16:09:22 -0700

    updated cloudbuild

--- backend/app/utils/get_secret.py
+++ backend/app/utils/get_secret.py
@@ -5,8 +5,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 def get_secret(secret_id):
-    """Retrieve a secret from Google Secret Manager."""
-    project_id = "your-google-cloud-project-id"
+    project_id = "tft-stocks"
     client = secretmanager.SecretManagerServiceClient()
     name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
     response = client.access_secret_version(request={"name": name})
